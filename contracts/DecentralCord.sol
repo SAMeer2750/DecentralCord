214c2f96a1efb098e0680b3a46be42018147f9fb
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-02-04 16:00:23 +0530

    Moderators of a server can create channels.
    Now moderators of that channel can create channels in that particular server.

--- contracts/DecentralCord.sol
+++ contracts/DecentralCord.sol
@@ -1,58 +1,73 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-contract discord {
-    struct Message {
-        address sender;
-    }
+contract discord{
 
-    struct Channel {
+    struct Message{
+        address sender;
+        string message;
+        uint256 timeStamp;
+    }
+    
+    struct Channel{
         uint256 channelId;
         string channelName;
     }
 
-    struct Server {
+    struct Server{
         uint256 serverId;
         string serverName;
         address Owner;
+
     }
 
-    mapping(uint256 => Server) public servers;
-    mapping(uint256 => Channel) public channels;
-    mapping(uint256 => Channel[]) public ServerChannels;
+    mapping(uint256 => Server) public servers;//server Id to Servers
+    mapping(uint256 => Channel) public channels;//channel Id to Channels
+    mapping(uint256 => Channel[]) public ServerChannels;//server Id to Channel
+    mapping(address => Server[]) public UserServers;//user Address to servers
+    mapping(uint256 => address[]) public moderators;//Server ID to moderators
+
 
     Server[] allServers;
     uint256 serverCount = 1;
     uint256 channelCount = 1;
 
-    function createServer(string memory _serverName) public {
+    function createServer(string memory _serverName)public{
         address owner = msg.sender;
         uint256 serverId = serverCount;
-        Server memory server = Server(serverId, _serverName, owner);
+        Server memory server = Server(serverId, _serverName, owner); 
         servers[serverId] = server;
         allServers.push(server);
         serverCount++;
     }
 
-    function createChannel(string memory _channelName, uint256 _serverId)
-        public
-    {
-        uint256 channelId = channelCount;
-        Channel memory channel = Channel(channelId, _channelName);
-        channels[channelId] = channel;
-        ServerChannels[_serverId].push(channel);
-        channelCount++;
+    function addModerators(address _mod, uint256 _serverId) public {
+        require(msg.sender == servers[_serverId].Owner , "You do not have the access to create channel");
+        moderators[_serverId].push(_mod);
+    }
+    
+    function createChannel(string memory _channelName, uint256 _serverId) public {
+        for(uint256 i = 0 ; i < moderators[_serverId].length ; i++){
+            if(msg.sender == servers[_serverId].Owner ||msg.sender == moderators[_serverId][i]){
+                uint256 channelId = channelCount;
+                Channel memory channel = Channel(channelId, _channelName);
+                channels[channelId] = channel;
+                ServerChannels[_serverId].push(channel);
+                channelCount++;
+            }
+        }    
     }
 
-    function getAllServers() public view returns (Server[] memory) {
-        return (allServers);
+    function enterServer(uint256 _serverId) public {
+        uint256 ServerID = _serverId;
+        UserServers[msg.sender].push(servers[ServerID]);
     }
 
-    function getChannels(uint256 _serverId)
-        public
-        view
-        returns (Channel[] memory)
-    {
-        return (ServerChannels[_serverId]);
+    function getAllServers() public view returns(Server[] memory){
+        return(allServers);
+    }
+    
+    function getChannels(uint256 _serverId) public view returns(Channel[] memory) {
+        return(ServerChannels[_serverId]);
     }
 }
