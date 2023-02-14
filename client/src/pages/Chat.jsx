caad387362c11d3b4eb83ee72f53693e7c9555ec
Ankit Gupta <92376931+xnki7@users.noreply.github.com>
2023-02-14 17:01:23 +0530

    Made accounts component

---
+++ client/src/pages/Chat.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Chat.css"
+import Accounts from '../components/Accounts'
+// import LeftNavBar from "../components/LeftNavBar"
+
+function Chat() {
+  return (
+    <>
+    {/* <LeftNavBar/> */}
+    {/* <div className='Chat'>Chat</div> */}
+    <Accounts/>
+    </>
+  )
+}
+
+export default Chat
