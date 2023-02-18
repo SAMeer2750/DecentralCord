c0ee89a276f10ed1fcd50a327833ce1a5b09f79a
Sameer Madhav D <sameermadhav.5@gmail.com>
2023-02-18 18:54:34 +0530

    .....

--- src/components/SignUpCard.jsx
+++ src/components/SignUpCard.jsx
@@ -3,13 +3,20 @@ import "./SignUpCard.css";
 import { Link } from "react-router-dom";
 import { ethers } from 'ethers';
 
-function SignUpCard({account,setAccount}) {
+function SignUpCard({account,setAccount,contract, setContract}) {
 
   const connectHandler = async()=>{
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
     const account = ethers.utils.getAddress(accounts[0])
     setAccount(account);
   }
+  // const signInButton = document.getElementById("sign_up");
+  // signInButton.addEventListener("click", handleSignIn);
+  // const handleSignIn = async ()=>{
+  //   const usernameInput = document.querySelector('input[type="text"]').value;
+  //   const enterServer = await contract.enterServer(usernameInput).send({ from: account })
+  // }
+  // signInButton.addEventListener("click", handleSignIn);
 
   return (
 
@@ -19,12 +26,25 @@ function SignUpCard({account,setAccount}) {
         <img src={require("./image_3.png")} />
         <h1 id="head">Sign In</h1>
         <div className="tagline"><p id="statement">Hurry, Sign in to become a part of Decentral Cord's family.</p></div>
-        <input type="text" placeholder="Username" />
+        <input type="text" id="username" placeholder="Username" />
         <Link to="/Chat"><button id="sign_up">Sign In</button></Link>
-    
+
         <hr />
+
         <p id="already_user">Already a User?</p>
-        <Link to="/Chat"><button id="connect_wallet">Connect Wallet</button></Link>
+
+        {
+          account ?(
+            <button id="connect_wallet">
+              {account.slice(0, 6) + '...' + account.slice(38, 42)}{" "}
+            </button>
+          ):(
+            <button id="connect_wallet" onClick={connectHandler}>Connect Wallet</button>
+          )
+        }
+
+        <Link to="/Chat">
+          <button id="connect_wallet_enter">Enter</button></Link>
       </div>
     </div>
   );
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import "./SignUp.css";
 import SignUpCard from "../components/SignUpCard";
 
-function SignUp({account,setAccount}) {
+function SignUp({account,setAccount,contract, setContract}) {
   return (
     <div className="SignUp">
       <img className="SignUpBackground" src="./bg_img.png" alt="" />
       <div className="SignUpContent">
-        <SignUpCard account={account} setAccount={setAccount}/>
+        <SignUpCard account={account} setAccount={setAccount} contract={contract} setContract={setContract}/>
       </div>
     </div>
   );
